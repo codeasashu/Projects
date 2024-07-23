fb4ca5c885567852bc0a85711a1c5921f2283dee
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2024-07-24 01:20:14 +0530

    wip

--- sockets/server.c
+++ sockets/server.c
@@ -1,15 +1,19 @@
-#include <asm-generic/socket.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #define MYPORT "3496"
 
 void handleData(int fd) {
-  char msg[10];
+  char msg[1024];
   recv(fd, msg, sizeof(msg), 0);
   printf("client said: %s", msg);
+  printf("Sleeping for 1 seconds...\n");
+  sleep(1);
+  char *response = {"OK"};
+  write(fd, response, strlen(response));
 }
 
 int main(int argc, char *argv[]) {
@@ -34,10 +38,17 @@ int main(int argc, char *argv[]) {
     printf("error listen() %s", strerror(rv));
   }
 
-  // Server is listening, now accept client request
-  int newfd =
-      accept(socketfd, (struct sockaddr *)&their_addr, &their_addr_size);
+  while (1) {
+    printf("accepting connection\n");
+    int newfd =
+        accept(socketfd, (struct sockaddr *)&their_addr, &their_addr_size);
+    if (newfd < 0) {
+      continue;
+    }
+    handleData(newfd);
+    close(newfd);
+  }
 
-  handleData(newfd);
+  // Server is listening, now accept client request
   return 0;
 }
